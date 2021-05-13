53cedd45f3043e870b6e1a5fd09dee25fad0839e
Fiorellaps <fiorellapiriz@hotmail.com>
2021-05-13 13:20:00 +0200

    First commit

---
+++ 1_introduccion/laboratorio/js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value = "Fiorella";
+document.getElementById("lastname").value = "Piriz Sapio";
+document.getElementById("avatar").src = "/img/avatar.png";
---
+++ 1_introduccion/playground/js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "Hola Javascript";
+console.log(2 + 2)
+console.log("%cMi super texto", "color:green; font-style:italic;");
